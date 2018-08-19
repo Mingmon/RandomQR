bb657a1287ab7d5846d940109204cefd8eccf2a2
Unknown <60010465@kmitl.ac.th>
2018-08-20 01:18:09 +0700

    add randQR

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 $(function() {
-  $('.button').click(function(){
-    $("#bg").attr('src',"img/qrcode2.png");
-    return false;
-  });
+$('.button').click(function() {
+  //alert("จะสุ่มล่ะนะ");
+  $("#bg").attr('src',"img/loading.gif");
+    setTimeout(function() {
+      var qrimg=["img/QRcode/เกม.png","img/QRcode/เซฟ.png","img/QRcode/เต้.png"
+        ,"img/QRcode/เนวัดดาว.png","img/QRcode/เบิร์ด.png","img/QRcode/เฟรม.png"
+        ,"img/QRcode/เฟิน.png","img/QRcode/เฟินธนัชพร.png","img/QRcode/เฟิร์นวรนิษฐา.png" ];
+      var imgRand = Math.floor((Math.random() * 9));
+      $("#bg").attr('src',qrimg[imgRand]);
+        },
+        2700);
+        return false;
+});
 });
