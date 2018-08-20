9ec3ce72a66bee97f64fcf589b673d7bb9b24a17
Unknown <60010465@kmitl.ac.th>
2018-08-21 02:35:02 +0700

    upup

--- js/backup.js
+++ js/backup.js
@@ -1,10 +1,9 @@
 $(function() {
 
-  $('.button').click(
-    function(){
-    var qrimg=["img/QRcode/เกม.png","img/QRcode/เซฟ.png","img/QRcode/เต้.png","img/QRcode/เนวัดดาว.png"];
+  $('.button').click(function() {
+    var qrimg = ["img/QRcode/เกม.png", "img/QRcode/เซฟ.png", "img/QRcode/เต้.png", "img/QRcode/เนวัดดาว.png"];
     var imgRand = Math.floor((Math.random() * 4));
-    $("#bg").attr('src',qrimg[imgRand]);
+    $("#bg").attr('src', qrimg[imgRand]);
     return false;
   });
 });
