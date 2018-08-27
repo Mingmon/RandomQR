6419ab984728beddbf837659f06763c61726a935
Nattanan Semathong <60010313@kmitl.ac.th>
2018-08-27 16:14:58 +0700

    remove backup and add all img

--- js/backup.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-  $('.button').click(function() {
-    var qrimg = ["img/QRcode/เกม.png", "img/QRcode/เซฟ.png", "img/QRcode/เต้.png", "img/QRcode/เนวัดดาว.png"];
-    var imgRand = Math.floor((Math.random() * 4));
-    $("#bg").attr('src', qrimg[imgRand]);
-    return false;
-  });
-});
